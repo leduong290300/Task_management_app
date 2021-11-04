7a435650ed13a8f3a62c1820b32efa15e19a17fb
leduong290300 <leduong290300@gmail.com>
2021-11-04 17:43:09 +0700

    devide-component

---
+++ client/src/components/TaskItems.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todo-item">
+    <div class="checker">
+      <span class="checker"><input type="checkbox" /></span>
+    </div>
+    <span class="content">Create theme</span>
+    <div class="btn-icon">
+      <b-icon
+        class="icon icon-edit"
+        icon="pencil-square"
+        variant="success"
+      ></b-icon>
+      <b-icon class="icon icon-trash" icon="trash" variant="danger"></b-icon>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TaskItems",
+};
+</script>
